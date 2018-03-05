4cb315cfa794cd43e13c44ff8eacd5e10cb6f04b
Murad <mhdmuradahmed@gmail.com>
2018-03-05 18:44:20 +0000

    added scraper class

--- AVATAR.py
+++ AVATAR.py
@@ -6,49 +6,11 @@ from EdgeDetection import EdgeDetection
 from ImageWarper import ImageWarper
 from ImageHasher import ImageHasher
 from CollectorDB import CollectorDB
-import requests
-from bs4 import BeautifulSoup
+from AVATAR.Scrape import Scrape
 from tkinter import *
 from tkinter import ttk
 
 p = print
-
-'''
-This method will scrape the web for a game that is NOT in the collectors database
-'''
-
-
-def scrape_game(game_name, real_name):
-    # generate request URL
-    url = "http://www.argos.co.uk/search/" + game_name + "ps4/"
-    # retrieve page
-    page = requests.get(url)
-    # parse page as HTML
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # find relevant span tags for each item required from html
-    price = soup.find('span', class_='ac-product-price__amount').get_text()
-    name = soup.find('div', class_='ac-product-name ac-product-card__name').get_text()
-    rating = soup.find('div', class_='ac-product-card__rating').get_text()
-    rating = rating.split(" ")
-    rating = rating[1]
-    rating = rating.split(".")
-    rating = rating[0]
-    lower_name = str(name).lower()
-    real_name = real_name.lower()
-    check_name = real_name.split(" ")
-    count = 0
-    # check if the game retrieved matches our search criteria
-    for words in check_name:
-        if words in lower_name:
-            count = count + 1
-    # if it does, inform user of the name, price and rating
-    if count == len(check_name):
-        print(name + " available for " + price + " at " + url)
-        print(rating, "out of 5 stars")
-        p("")
-    else:
-        print("Unable to find game at " + url)
-        p("")
 
 
 ''' 
@@ -72,7 +34,7 @@ def get_warped_hash(event, x_cords, y_cords, flag, void):
             p('Game is in collectors database.')
             p('')
         elif name is not "":
-            scrape_game(search_query, name)
+            scraper.scrape_game(search_query, name)
 
 
 # todo add GUI function
@@ -84,6 +46,7 @@ if __name__ == '__main__':
     edge_detection = EdgeDetection()
     image_warper = ImageWarper()
     image_hasher = ImageHasher()
+    scraper = Scrape()
 
     choice = int(input("What would you like to do? 1)View own collection 2)Scan a game"))
     if choice == 1:
